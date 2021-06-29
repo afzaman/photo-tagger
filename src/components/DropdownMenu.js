6093fceaea4a667efa71b813204411f5c596591c
afzaman <afzaman2003@gmail.com>
2021-06-29 13:45:36 -0400

    added tags for images and random 3 options

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -1,6 +1,22 @@
+import memeTags from '../images/memeTags.js'
+
 function DropdownMenu({xPos, yPos}){
 
     // const [xPos, yPos] = props
+
+    function getRandom(arr, n) {
+        var result = new Array(n),
+            len = arr.length,
+            taken = new Array(len);
+        if (n > len)
+            throw new RangeError("getRandom: more elements taken than available");
+        while (n--) {
+            var x = Math.floor(Math.random() * len);
+            result[n] = arr[x in taken ? taken[x] : x];
+            taken[x] = --len in taken ? taken[len] : len;
+        }
+        return result;
+    }
 
     return(
         <div
@@ -8,9 +24,11 @@ function DropdownMenu({xPos, yPos}){
             style={{ top: `calc(${yPos}px)`, left: `${xPos}px` }}
         >
             <ul>
-                <li>Meme 1</li>
-                <li>Meme 2</li>
-                <li>Meme 3</li>
+                {getRandom(memeTags, 3).map((tag) => 
+                    <button key={tag.x + tag.y}>
+                        {tag.label}
+                    </button>
+                )}
             </ul>
         </div>
     )
---
+++ src/images/memeTags.js
@@ -0,0 +1,139 @@
+const memeTags = [
+    {
+      "x": 0,
+      "y": 0,
+      "label": "KND #2"
+    },
+    {
+      "x": 540,
+      "y": 0,
+      "label": "That’s a Paddlin"
+    },
+    {
+      "x": 1480,
+      "y": 0,
+      "label": "Where's my Drink?"
+    },
+    {
+      "x": 2793,
+      "y": 0,
+      "label": "Red Angry Bird"
+    },
+    {
+      "x": 3940,
+      "y": 0,
+      "label": "Doug Dimmidome"
+    },
+    {
+      "x": 6293,
+      "y": 0,
+      "label": "Oh Yeah!"
+    },
+    {
+      "x": 753,
+      "y": 727,
+      "label": "City Wok"
+    },
+    {
+      "x": 4853,
+      "y": 1040,
+      "label": "It's all coming together"
+    },
+    {
+      "x": 5540,
+      "y": 1994,
+      "label": "Is this a ____?"
+    },
+    {
+      "x": 2593,
+      "y": 1814,
+      "label": "Pewdiepie"
+    },
+    {
+      "x": 1020,
+      "y": 2334,
+      "label": "Grimm"
+    },
+    {
+      "x": 0,
+      "y": 3467,
+      "label": "Danny Phantom"
+    },
+    {
+      "x": 2286,
+      "y": 3460,
+      "label": "Waluigi"
+    },
+    {
+      "x": 3746,
+      "y": 3346,
+      "label": "Wallace"
+    },
+    {
+      "x": 4866,
+      "y": 4393,
+      "label": "Manray"
+    },
+    {
+      "x": 2860,
+      "y": 4280,
+      "label": "Bob Ross"
+    },
+    {
+      "x": 2153,
+      "y": 4420,
+      "label": "Ricardo"
+    },
+    {
+      "x": 0,
+      "y": 5226,
+      "label": "Naruto"
+    },
+    {
+      "x": 560,
+      "y": 6152,
+      "label": "Harambe"
+    },
+    {
+      "x": 2900,
+      "y": 5479,
+      "label": "Surprised Pikachu"
+    },
+    {
+      "x": 5000,
+      "y": 6379,
+      "label": "Big Chungus"
+    },
+    {
+      "x": 7256,
+      "y": 6045,
+      "label": "Sans"
+    },
+    {
+      "x": 6049,
+      "y": 5272,
+      "label": "CJ"
+    },
+    {
+      "x": 5955,
+      "y": 7059,
+      "label": "Elon"
+    },
+    {
+      "x": 1995,
+      "y": 6932,
+      "label": "Stan Lee"
+    },
+    {
+      "x": 4335,
+      "y": 5525,
+      "label": "Saitama"
+    },
+    {
+      "x": 2829,
+      "y": 3232,
+      "label": "Ugandan Knuckles"
+    }
+  ]
+
+export default memeTags
